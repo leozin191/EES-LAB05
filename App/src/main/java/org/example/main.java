6fd49b4a5e5ecf060978961ebe2dbfadb42f41c6
leog <leonardo_gorle@hotmail.com>
2023-09-08 21:20:35 -0300

    creating

---
+++ App/src/main/java/org/example/main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        //inicio do programa
+        System.out.println("Olá!");
+        System.out.println("Engenharia Econômica para Software - Leonardo Gorle Almeida");
+
+        //enquanto i menor que 10 irá executar print
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
